23703ff5c0b8eb5bfac0aa76b85a919ba9c723ac
Erik Godoy <erik.sgodoy@gmail.com>
2021-05-14 00:49:27 -0300

    OrderItem, many-to-many association with extra attributes PART2

--- src/main/java/com/example/curse/config/TesteConfig.java
+++ src/main/java/com/example/curse/config/TesteConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.example.curse.entities.Category;
 import com.example.curse.entities.Order;
+import com.example.curse.entities.OrderItem;
 import com.example.curse.entities.Product;
 import com.example.curse.entities.User;
 import com.example.curse.enums.OrderStatus;
 import com.example.curse.resources.repositories.CategoryRepository;
+import com.example.curse.resources.repositories.OrderItemRepository;
 import com.example.curse.resources.repositories.OrderRepository;
 import com.example.curse.resources.repositories.ProductRepository;
 import com.example.curse.resources.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TesteConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productReopository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -71,6 +76,13 @@ public class TesteConfig implements CommandLineRunner {
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		OrderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
 		
 	}
--- src/main/java/com/example/curse/entities/Order.java
+++ src/main/java/com/example/curse/entities/Order.java
@@ -2,6 +2,8 @@ package com.example.curse.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.example.curse.enums.OrderStatus;
@@ -31,6 +34,9 @@ public class Order implements Serializable{
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
 
 	public Order() {
 	}
@@ -42,6 +48,7 @@ public class Order implements Serializable{
 		setOrderStatus(orderStatus);
 		this.client = client;
 	}
+	
 
 	public Long getId() {
 		return id;
@@ -76,6 +83,10 @@ public class Order implements Serializable{
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+	
+	public Set<OrderItem> getItems(){
+		return items;
 	}
 
 	@Override
