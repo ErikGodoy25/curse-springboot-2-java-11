af993cf520e99354b27489c3ba9d453cb8823071
Erik Godoy <erik.sgodoy@gmail.com>
2021-05-19 09:03:14 -0300

    Payment, one-to-one association

--- src/main/java/com/example/curse/entities/Payment.java
+++ src/main/java/com/example/curse/entities/Payment.java
@@ -7,8 +7,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_payment")
@@ -21,7 +24,9 @@ public class Payment  implements Serializable {
 	
 	private Instant moment;
 	
+	@JsonIgnore
 	@OneToOne
+	@MapsId
 	private Order order;
 	
 	public Payment() {
